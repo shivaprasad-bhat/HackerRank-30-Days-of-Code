675d7130e5404aa8c75a7579c97b57671fc6e4a4
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-07-16 22:09:23 +0530

    Adding Operators solution

---
+++ src/day3/Operators.kt
@@ -0,0 +1,21 @@
+package day3
+
+/**
+ * @author shivaprasad bhat
+ */
+fun solve(meal_cost: Double, tip_percent: Int, tax_percent: Int): Unit {
+    val tip: Double = meal_cost * (tip_percent/100.0)
+    val tax: Double = meal_cost * (tax_percent/100.0)
+    val total: Long = Math.round(meal_cost+tip+tax)
+    println(total.toInt())
+}
+
+fun main(args: Array<String>) {
+    val meal_cost = readLine()!!.trim().toDouble()
+
+    val tip_percent = readLine()!!.trim().toInt()
+
+    val tax_percent = readLine()!!.trim().toInt()
+
+    solve(meal_cost, tip_percent, tax_percent)
+}
