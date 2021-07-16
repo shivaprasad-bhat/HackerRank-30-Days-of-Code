5276ceb0cea3f61044854835b5572d6b02a6179f
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-07-16 21:42:33 +0530

    adding Hello World solution

---
+++ src/day0/HelloWorld.kt
@@ -0,0 +1,13 @@
+package day0
+
+/**
+ * The instructions are Java-based, but we support submissions in many popular languages.
+ * You can switch languages using the drop-down menu above your editor,
+ * and the inputString variable may be written differently,
+ * depending on the best-practice conventions of your submission language
+ */
+fun main() {
+    val strMessage = readLine()
+    println("Hello, World.")
+    println("$strMessage")
+}
